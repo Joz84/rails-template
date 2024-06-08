c8030f186f08e66e405e795df8ed5f02ff9ad855
Jonathan Serafini <jonathan.serafini@gmail.com>
2024-06-08 17:58:24 +0200

    Update complete.rb

--- complete.rb
+++ complete.rb
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 ruby '#{RUBY_VERSION}'
 gem 'rails', "7.1"
 gem 'pg', "~> 1.1"
-gem "puma", "~> 6.0"
+gem "puma", "6.0"
 gem 'bootstrap', '~> 5.1.3'
 gem 'devise'
 gem 'pundit'
